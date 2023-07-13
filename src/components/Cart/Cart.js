0f88ee878802d2aed011910c93d81a28ac05c61e
Suraj.S <surajsuri279@gmail.com>
2023-07-13 20:44:33 +0530

    modified with some changes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Nav from "../Fruits/Nav";
+import Slider from "../Fruits/Slider";
 
 export default function Cart() {
   return (
     <>
-      <h1>Cart</h1>
+      <Nav />
     </>
   );
 }
